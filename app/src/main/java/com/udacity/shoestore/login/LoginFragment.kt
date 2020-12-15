959405b072517a4d5cc46d690c4e4d6610b63ad1
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-15 23:16:15 +0200

    feat: Create an empty login screen

---
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
