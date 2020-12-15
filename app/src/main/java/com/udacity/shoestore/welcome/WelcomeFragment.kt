7e5e10e6ef660845a6a4f07d48587200131cc4fe
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-16 01:05:43 +0200

    feat: Navigate to the instruction screen

--- app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment: Fragment() {
@@ -13,6 +14,10 @@ class WelcomeFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+        binding.nextButton.setOnClickListener {
+            it.findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment())
+        }
 
         return binding.root
     }
