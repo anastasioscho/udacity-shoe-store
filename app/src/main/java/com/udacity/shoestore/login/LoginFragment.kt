1ce6cce593e26733742b61efe2a0b99dc0ed005a
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-16 01:01:31 +0200

    feat: Navigate to the welcome screen

--- app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment() {
@@ -13,6 +14,14 @@ class LoginFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.loginButton.setOnClickListener {
+            it.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.createAccountButton.setOnClickListener {
+            it.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
 
         return binding.root
     }
