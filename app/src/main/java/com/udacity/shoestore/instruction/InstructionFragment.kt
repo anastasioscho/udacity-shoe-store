d9f5076dcedf58a21c4b99f5d6f932009eecc32c
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-16 01:47:32 +0200

    feat: Navigate to the show list fragment

--- app/src/main/java/com/udacity/shoestore/instruction/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/instruction/InstructionFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
 class InstructionFragment: Fragment() {
@@ -14,6 +15,10 @@ class InstructionFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = FragmentInstructionBinding.inflate(inflater, container, false)
+
+        binding.startButton.setOnClickListener {
+            it.findNavController().navigate(InstructionFragmentDirections.actionInstructionFragmentToShoeListFragment())
+        }
 
         return binding.root
     }
