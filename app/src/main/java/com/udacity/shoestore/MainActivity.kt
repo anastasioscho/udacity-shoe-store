9a8b96ebb88165d229bf3626b3b77396b44773d7
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-15 23:01:01 +0200

    style: Format the code

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
