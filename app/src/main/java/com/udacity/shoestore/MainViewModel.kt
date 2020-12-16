e1d2108ee92ceb9426c6c79105c203ba73f0b946
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-16 05:47:16 +0200

    feat: Change the sample shoes

--- app/src/main/java/com/udacity/shoestore/MainViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/MainViewModel.kt
@@ -72,15 +72,8 @@ class MainViewModel: ViewModel() {
     private fun createSampleShoes(): List<Shoe> {
         val tempShoes = mutableListOf<Shoe>()
 
-        tempShoes.add(Shoe("Shoe 1", 10.0, "Company 1", "Description 1"))
-        tempShoes.add(Shoe("Shoe 2", 20.0, "Company 2", "Description 2"))
-        tempShoes.add(Shoe("Shoe 3", 30.0, "Company 3", "Description 3"))
-        tempShoes.add(Shoe("Shoe 4", 40.0, "Company 4", "Description 4"))
-        tempShoes.add(Shoe("Shoe 5", 50.0, "Company 5", "Description 5"))
-        tempShoes.add(Shoe("Shoe 6", 60.0, "Company 6", "Description 6"))
-        tempShoes.add(Shoe("Shoe 7", 70.0, "Company 7", "Description 7"))
-        tempShoes.add(Shoe("Shoe 8", 80.0, "Company 8", "Description 8"))
-        tempShoes.add(Shoe("Shoe 9", 90.0, "Company 9", "Description 9"))
+        tempShoes.add(Shoe("Originals Vegan Samba", 36.0, "adidas", "Originals Vegan Samba trainers in white"))
+        tempShoes.add(Shoe("Nike ZoomX SuperRep Surge", 38.0, "Nike", "Very comfortable"))
 
         return tempShoes
     }
