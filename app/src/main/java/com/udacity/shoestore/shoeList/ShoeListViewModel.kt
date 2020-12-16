39087bb1d46a7285ea13c7d1cc0e1538a2df0d3f
Anastasios Chondrogiannis <anastasioscho@me.com>
2020-12-16 02:11:15 +0200

    feat: Create a few shoes

--- app/src/main/java/com/udacity/shoestore/shoeList/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/shoeList/ShoeListViewModel.kt
@@ -2,11 +2,29 @@ package com.udacity.shoestore.shoeList
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import timber.log.Timber
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListViewModel(application: Application): AndroidViewModel(application) {
 
+    private val _shoes = MutableLiveData<List<Shoe>>()
+    val shoes: LiveData<List<Shoe>>
+        get() = _shoes
+
     init {
-        Timber.tag("hello").d("ShoeListViewModel initialized")
+        _shoes.value = createSampleShoes()
+    }
+
+    private fun createSampleShoes(): List<Shoe> {
+        val tempShoes = mutableListOf<Shoe>()
+
+        tempShoes.add(Shoe("Shoe 1", 10.0, "Company 1", "Description 1"))
+        tempShoes.add(Shoe("Shoe 2", 20.0, "Company 2", "Description 2"))
+        tempShoes.add(Shoe("Shoe 3", 30.0, "Company 3", "Description 3"))
+        tempShoes.add(Shoe("Shoe 4", 40.0, "Company 4", "Description 4"))
+        tempShoes.add(Shoe("Shoe 5", 50.0, "Company 5", "Description 5"))
+
+        return tempShoes
     }
 }
